4e55453e5db34ece769fed893433c8aa4d4e07d9
j9210 <71297542+j9210@users.noreply.github.com>
2020-12-08 20:27:54 -0600

    add PORT specifications

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 const { animals } = require('./data/animals');
 
@@ -50,7 +51,7 @@ app.get('/api/animals', (req, res) => {
   });
 
 // method to make server listen
-app.listen(3001, () => {
-  console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
 
